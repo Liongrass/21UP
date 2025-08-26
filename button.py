de2213f3c87bf958064aebc7cb790dd497f2ddb3
Leo <2104935+Liongrass@users.noreply.github.com>
2025-08-25 22:10:03 -0700

    new button logic that listens to multiple buttons in once function

--- button.py
+++ button.py
@@ -6,24 +6,29 @@ import asyncio
 # Functions and variables
 from payments import payment, stop_process
 
-pinIn=17
+pinInA=17
+pinInB=27
+pinInC=22
+
+pin_list = [17, 27, 22]
+counts = { ch: 0 for ch in pin_list }
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pinIn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(pin_list, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-
-# Assume the button is not currently pressed
-was_pressed = True
-
-# Function that waits for the button to be pressed
 async def listener():
     while True:
-        is_pressed = GPIO.input(pinIn)
-        if is_pressed and not was_pressed:
-            print("Button was pressed!")
-            sleep(0.5)
-            stop_process()
-            await payment()
-            sleep(0.5)
-        was_pressed = is_pressed
+        detected = False
+        for ch in pin_list:
+            if GPIO.input(ch):
+                detected = True
+                counts[ch] += 1
+        if detected:
+            print(counts)
+            for ch in pin_list:
+                counts[ch] = 0
+        # if an event remains high for more than 0.5 sec it might
+        # be counted again on the next loop. Likewise if an event
+        # comes and goes before the next loop it will be missed.
+        sleep(0.5)
