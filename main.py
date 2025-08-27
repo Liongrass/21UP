a613d43c9d00e0908205206fe550694bf448ba12
Leo <2104935+Liongrass@users.noreply.github.com>
2025-08-26 22:14:49 -0700

    edit language in default env file

--- main.py
+++ main.py
@@ -8,24 +8,3 @@ from button import listener
 
 # The main function is run, waiting for button to be pressed
 asyncio.run(listener())
-
-#asyncio.run(main())
-'''
-async def main():
-    tasks = [
-        asyncio.create_task(listenerA()),
-        asyncio.create_task(listenerB()),
-    ]
-    #await asyncio.gather(*tasks)
-
-asyncio.run(main())
-'''
-
-#listener()
-#async def main():
-#	await asyncio.gather(asyncio.create_task(listener()))
-
-#async def main():
-#    await asyncio.gather(asyncio.create_task(listener()), asyncio.create_task(payment()))
-
-#asyncio.run(main())
