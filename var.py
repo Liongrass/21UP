5c58e788ae78d05e16adeaa46ca5e798bf548d4b
Leo <2104935+Liongrass@users.noreply.github.com>
2025-09-01 21:27:44 -0700

    add logging file

--- var.py
+++ var.py
@@ -1,8 +1,9 @@
-import os
+# Modules
+from dotenv import load_dotenv
 import json
 import logging
+import os
 import traceback
-from dotenv import load_dotenv
 
 ##### VARIABLES #####
 
@@ -15,7 +16,9 @@ memo_str = os.getenv("MEMO_STRING", "Thank you for your purchase from 21UP!")
 expiry = int(os.getenv("INVOICE_EXPIRY", 60))
 
 debuglevel = os.getenv("DEBUG_LEVEL", "INFO")
-logging.basicConfig(level=debuglevel)
+
+logging.basicConfig(filename='21UP.log', format='%(asctime)s %(levelname)s %(message)s', level=debuglevel)
+logging.info(f"Setting debug level at {debuglevel}")
 
 ##### TRAYS #####
 
@@ -31,6 +34,13 @@ amount   = [tray0[1], tray1[1], tray2[1], tray3[1], tray4[1], tray5[1]]
 unit     = [tray0[2], tray1[2], tray2[2], tray3[2], tray4[2], tray5[2]]
 pin_in   = [tray0[3], tray1[3], tray2[3], tray3[3], tray4[3], tray5[3]]
 pin_out  = [tray0[4], tray1[4], tray2[4], tray3[4], tray4[4], tray5[4]]
+
+logging.debug(f"Labels: {label}")
+logging.debug(f"Amounts: {amount}")
+logging.debug(f"Unit: {unit}")
+logging.debug(f"Pin In: {pin_in}")
+logging.debug(f"Pin Out: {pin_out}")
+
 
 # pinOut=5  # CH1
 # pinOut=6  # CH2
