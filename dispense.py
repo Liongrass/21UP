54801735cec88c6c9bb2754d3e203afb4f5a1a32
Leo <2104935+Liongrass@users.noreply.github.com>
2025-12-06 14:56:12 -0800

    remove hot pin

--- dispense.py
+++ dispense.py
@@ -4,14 +4,11 @@ from gpiozero import OutputDevice
 from time import sleep
 
 # Functions and variables
-from var import pin_hot, pin_out, relay_duration
+from var import pin_out, relay_duration
 
 device = [OutputDevice(pin, initial_value=False) for pin in pin_out]
-hotpin = OutputDevice(pin_hot, initial_value=False)
 
 def trigger(pinOut, tray):
-	hotpin.on()
-	logging.debug(f"Making machine hot at pin {pin_hot}")
 	logging.debug(device[tray])
 	device[tray].on()
 	logging.info(f"Dispensing tray {tray} at pin {pin_out[tray]}")
@@ -19,5 +16,3 @@ def trigger(pinOut, tray):
 	sleep(relay_duration)
 	device[tray].off()
 	logging.info(f"Done dispensing tray {tray}")
-	hotpin.off()
-	logging.debug(f"Making machine cold at pin {pin_hot}")
