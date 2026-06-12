a42907f94635b6d48a053519e4d05a0bc8d783b4
Leo <leonhard@weese.ws>
2026-06-11 20:43:27 -0700

    log temperature

--- accounting.py
+++ accounting.py
@@ -5,10 +5,11 @@ import os
 
 
 # Functions and variables
+from barometer import get_barometrics
 from var import amount, display_expiry, suceess_screen_expiry, expiry, label, lnbits_server, memo_str, pin_out, show_display, unit, x_api_key
 #from var import amount, display_expiry, suceess_screen_expiry, expiry, label, lnbits_server, memo_str, pin_out, show_display, unit, x_api_key
 
-fieldnames = ['settled', 'date', 'tray', 'item', 'price', 'currency', 'time_to_pay', 'payment_hash']
+fieldnames = ['settled', 'date', 'tray', 'item', 'price', 'currency', 'temp', 'time_to_pay', 'payment_hash']
 
 def check_csv():
     exists = os.path.isfile('pic/sales.csv')
@@ -29,22 +30,16 @@ def amend_csv(settled, invoice_created, tray, invoice_paid, payment_hash):
         time_to_pay = "N/A"
     else:
         time_to_pay = invoice_paid - invoice_created
+    t = get_barometrics()
     data = {'settled': settled,
             'date': invoice_created,
             'tray': tray,
             'item': label[tray],
             'price': amount[tray],
             'currency': unit[tray],
+            'temp': t,
             'time_to_pay': time_to_pay,
             'payment_hash': payment_hash}
     with open('pic/sales.csv', mode='a', newline='') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         csv_writer.writerow(data)
-
-#def write_csv(filename, data):
-#    fieldnames = ['title', 'minsat', 'maxsat', 'comment', 'id', 'lnurl']
-#    with open('csv/'+filename, mode='w') as csv_file:
-#        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-#        csv_writer.writeheader()
-#        for row in data:
-#            csv_writer.writerow(row)
